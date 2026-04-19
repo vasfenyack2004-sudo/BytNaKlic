b923dc91403d33da2fdfaeb7118794f1667682bc
martin <martin@MacBook-Pro.local>
2026-04-19 10:45:35 +0200

    fix: simplify babel and disable experimental compiler

--- babel.config.js
+++ babel.config.js
@@ -1,11 +1,6 @@
-mmodule.exports = function(api) {
+module.exports = function(api) {
   api.cache(true);
   return {
     presets: ['babel-preset-expo'],
-    plugins: [
-      // Цей плагін допоможе уникнути помилок з нативними модулями у вебі
-      '@babel/plugin-proposal-export-namespace-from',
-      'react-native-reanimated/plugin',
-    ],
   };
 };
